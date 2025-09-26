d69814dff91e46f3f4efa704f0d3f18493599e6f
Dan-Fuentes99 <fuentes14@icloud.com>
2025-09-26 13:05:47 -0400

    Added animations to Home and Explore.

--- src/components/home/NewItems.jsx
+++ src/components/home/NewItems.jsx
@@ -40,7 +40,7 @@ const NewItems = () => {
   return (
     <section id="section-items" className="no-bottom">
       <div className="container">
-        <div className="row">
+        <div className="row wow fadeIn">
           <div className="col-lg-12">
             <div className="text-center">
               <h2>New Items</h2>
