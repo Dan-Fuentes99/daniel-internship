12923c46b1ee52d8d26f20e37cd61de14d4fe0c3
Dan-Fuentes99 <fuentes14@icloud.com>
2025-09-26 11:13:36 -0400

    Added dynamic function, Load More button, and filer to Explore Items.

--- src/components/explore/ExploreItems.jsx
+++ src/components/explore/ExploreItems.jsx
@@ -1,20 +1,47 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import AuthorImage from "../../images/author_thumbnail.jpg";
-import nftImage from "../../images/nftImage.jpg";
+import axios from "axios";
+import Timer from "../Timer";
+import Skeleton from "../UI/Skeleton";
 
 const ExploreItems = () => {
+  const [itemCount, setItemCount] = useState(8);
+  const [exploreItems, setExploreItems] = useState([]);
+  const [skeleton, setSkeleton] = useState(true);
+
+  const fetchExploreItems = async () => {
+    const response = await axios.get('https://us-central1-nft-cloud-functions.cloudfunctions.net/explore');
+    setExploreItems(response.data);
+  };
+
+  async function handleFilter(filter) {
+    setSkeleton(false);
+    const response = await axios.get(`https://us-central1-nft-cloud-functions.cloudfunctions.net/explore?filter=${filter}`);
+    setExploreItems(response.data);
+
+    setExploreItems(response.data);
+    setSkeleton(true);
+  }
+
+  useEffect(() => {
+    fetchExploreItems();
+  }, []);
+
   return (
     <>
       <div>
-        <select id="filter-items" defaultValue="">
+        <select 
+        id="filter-items" 
+        defaultValue="" 
+        onChange={(e) => handleFilter(e.target.value)}>
           <option value="">Default</option>
           <option value="price_low_to_high">Price, Low to High</option>
           <option value="price_high_to_low">Price, High to Low</option>
           <option value="likes_high_to_low">Most liked</option>
         </select>
       </div>
-      {new Array(8).fill(0).map((_, index) => (
+      {exploreItems.length && skeleton ? (
+        exploreItems.slice(0, itemCount).map((item, index) => (
         <div
           key={index}
           className="d-item col-lg-3 col-md-6 col-sm-6 col-xs-12"
@@ -23,15 +50,15 @@ const ExploreItems = () => {
           <div className="nft__item">
             <div className="author_list_pp">
               <Link
-                to="/author"
+                to={`author/${item.authorId}`}
                 data-bs-toggle="tooltip"
                 data-bs-placement="top"
               >
-                <img className="lazy" src={AuthorImage} alt="" />
+                <img className="lazy" src={item.authorImage} alt="" />
                 <i className="fa fa-check"></i>
               </Link>
             </div>
-            <div className="de_countdown">5h 30m 32s</div>
+            {item.expiryDate && (<Timer expiryTime={item.expiryDate} />)}
 
             <div className="nft__item_wrap">
               <div className="nft__item_extra">
@@ -51,27 +78,40 @@ const ExploreItems = () => {
                   </div>
                 </div>
               </div>
-              <Link to="/item-details">
-                <img src={nftImage} className="lazy nft__item_preview" alt="" />
+              <Link to={`item-details/${item.nftId}`}>
+                <img src={item.nftImage} className="lazy nft__item_preview" alt="" />
               </Link>
             </div>
             <div className="nft__item_info">
-              <Link to="/item-details">
-                <h4>Pinky Ocean</h4>
+              <Link to={`item-details/${item.nftId}`}>
+                <h4>{item.title}</h4>
               </Link>
-              <div className="nft__item_price">1.74 ETH</div>
+              <div className="nft__item_price">{item.price} ETH</div>
               <div className="nft__item_like">
                 <i className="fa fa-heart"></i>
-                <span>69</span>
+                <span>{item.likes}</span>
               </div>
             </div>
           </div>
         </div>
-      ))}
+      ))) : (
+        <>
+          {new Array(8).fill(0).map((_, index) => (
+            <div key={index} className="col-lg-3 col-md-6 col-sm-6 col-xs-12">
+              <Skeleton width="100%" height="400px" />
+            </div>
+          ))}
+        </>
+      )}
       <div className="col-md-12 text-center">
-        <Link to="" id="loadmore" className="btn-main lead">
+        {itemCount !== 16 && (
+         <Link
+         onClick={() => setItemCount(itemCount + 4)}
+         to=""
+         id="loadmore" 
+         className="btn-main lead">
           Load more
-        </Link>
+        </Link>)}
       </div>
     </>
   );
